e7ad7724708bbb874a59a3945b5ebc8b9745aad6
sneka9102000 <snekakumar925@gmail.com>
2022-07-18 12:30:31 +0530

    commit

--- front-end/src/component/layout/Header/Header.js
+++ front-end/src/component/layout/Header/Header.js
@@ -4,10 +4,7 @@ import { ReactNavbar} from "overlay-navbar"
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import {Link, useNavigate} from 'react-router-dom'
-import { useSelector } from "react-redux";
-
-
+import {Link} from 'react-router-dom'
 
 const options = {
   burgerColorHover: "#eb4034",
@@ -36,17 +33,16 @@ const options = {
 
 const Header = () => {
   
-
   return (
   <div>
    <div style={{display:"inline",position:"absolute",float:"right",margin:"0 0 0 80%",padding:"0 0 0 0",height:"100px"}}>
     <nav style={{float:"right",display:"inline",margin:"30% 0 0 0"}}> 
-  <nav style={{float:"right",display:"inline"}}>
-  <Link to="/search"><SearchIcon style={{fontSize:"50px"}}/></Link>
-  <Link to='/cart' ><ShoppingCartIcon style={{fontSize:"50px"}}/></Link>
-  <Link to="/login"><AccountCircleIcon style={{fontSize:"50px"}}/></Link>
-   </nav>
- </nav> 
+    <nav style={{float:"right",display:"inline"}}>
+    <Link to="/search"><SearchIcon style={{fontSize:"50px"}}/></Link>
+    <Link to='/cart' ><ShoppingCartIcon style={{fontSize:"50px"}}/></Link>
+    <Link to="/login"><AccountCircleIcon style={{fontSize:"50px"}}/></Link>
+    </nav>
+    </nav> 
 
     <ReactNavbar {...options} />
         
