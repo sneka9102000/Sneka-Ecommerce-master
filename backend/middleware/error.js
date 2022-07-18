484b643515f9591f0cc42843e0cc4d1819976386
sneka9102000 <snekakumar925@gmail.com>
2022-07-18 12:06:47 +0530

    commit

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,7 +10,7 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
-    // Mongoose duplicate key error
+    // Database duplicate key error
   if (err.code === 11000) {
     const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
     err = new ErrorHandler(message, 400);
