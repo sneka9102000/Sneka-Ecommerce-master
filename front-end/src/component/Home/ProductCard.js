55b601f834882d8f4b707330b7af94581f9c5349
sneka9102000 <snekakumar925@gmail.com>
2022-07-18 19:39:53 +0530

    commit

--- front-end/src/component/Home/ProductCard.js
+++ front-end/src/component/Home/ProductCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { Rating } from "@material-ui/lab";
 import ReactStars from "react-rating-stars-component";
 
 const ProductCard = ({ product }) => {
