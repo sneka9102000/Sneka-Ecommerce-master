50acd6170b98bbf578a8ee975c5b09d74234ff4d
sneka9102000 <snekakumar925@gmail.com>
2022-07-18 22:29:14 +0530

    commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import Header from "./component/layout/Header/Header.js";
 import React from "react";
-import Footer from "./component/layout/Footer/Footer.js"
 import Home from "./component/Home/Home";
 import ProductDetails from "./component/Product/ProductDetails.js";
 import Products from "./component/Product/Products.js";
